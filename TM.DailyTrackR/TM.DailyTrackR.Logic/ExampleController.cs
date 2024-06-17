5bc782ddd235e35d2d2af03cf02770590079beae
Lehelbalint <balint.lehel@student.ms.sapientia.ro>
2024-06-17 19:40:35 +0300

    Upload Project+ Homework Day1

---
+++ TM.DailyTrackR/TM.DailyTrackR.Logic/ExampleController.cs
@@ -0,0 +1,48 @@
+﻿namespace TM.DailyTrackR.Logic
+{
+	using System.Data;
+	using System.Data.SqlClient;
+
+  public sealed class ExampleController
+  {
+    string connectionString = @"Server=.\TM_DAILY_TRACKR;Database=TRACKR_DATA;Integrated Security=true;";
+
+    public int GetDataExample()
+    {
+            //string procedureName = "tm.GetAllProjectTypes";
+            string insertProcedure = "tm.InsertProjectType";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    using (SqlCommand command = new SqlCommand(insertProcedure, connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        connection.Open();
+                       command.Parameters.AddWithValue("@Description", "TestFromApp");
+						//command.Parameters.AddWithValue("@param2", value2);
+
+						command.ExecuteNonQuery();
+
+                        Console.WriteLine("Row inserted");
+						//SqlDataReader reader = command.ExecuteReader();
+						//Dictionary<int, string> result = new();
+						//while (reader.Read())
+						//{
+						//  result.Add((int)reader["project_type_id"], (string)reader["project_type_description"]);
+						//}
+					}  ;
+                   // string version = (string)command.ExecuteScalar();
+                    //System.Diagnostics.Debug.WriteLine("SQL Server Version: " + version);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An error occurred: " + ex.Message);
+                }
+            };
+
+      return 0;
+    }
+  }
+}
